b5102a6e44eade9a4ae5da64028b0939e16b3e8c
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-22 19:40:02 +0300

    JEE Unit 5.1 Homework: Library

--- src/main/java/com/company/calculator/Numbers.java
+++ src/main/java/com/company/calculator/Numbers.java
@@ -6,14 +6,18 @@ import java.util.List;
  * Created by Yevhen on 22.04.2016.
  */
 public class Numbers {
+    public static double parseNumber(String s) {
+        return Double.parseDouble(s);
+    }
+
     public static boolean isDoubleOperation(int operandCount, List<String> operandList) {
         // If <operandCount> < 0, th count of operands will not be checked
-        boolean result = (operandCount < 0) || operandList.size() == operandCount;
+        boolean result = (operandCount < 0) || (operandList.size() == operandCount);
 
         if (result) {
             // Try to parse each operand as Double
             try {
-                operandList.stream().forEach(Double::parseDouble);
+                operandList.stream().forEach(Numbers::parseNumber);
             } catch (NullPointerException | NumberFormatException e) {
                 result = false;
             }
