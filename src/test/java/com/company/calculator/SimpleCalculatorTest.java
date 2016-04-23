5d2eff4f6289b96d59107cac2ce0a15e99250e9b
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 13:18:01 +0300

    JEE Unit 5.1 Homework: Library

---
+++ src/test/java/com/company/calculator/SimpleCalculatorTest.java
@@ -0,0 +1,16 @@
+package com.company.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Yevhen on 23.04.2016.
+ */
+public class SimpleCalculatorTest extends ExpressionTest {
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+}
