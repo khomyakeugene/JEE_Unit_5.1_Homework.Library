c0e33b32608944b5fdab25f1a29964a4109c331b
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 19:31:04 +0300

    JEE Unit 5.1 Homework: Library

--- src/test/java/com/company/calculator/NumberAdditionTest.java
+++ src/test/java/com/company/calculator/NumberAdditionTest.java
@@ -12,12 +12,7 @@ public class NumberAdditionTest extends NumberTest {
     }
 
     @Override
-    protected int calcIntExpected(int operand_1, int operand_2) {
-        return operand_1 + operand_2;
-    }
-
-    @Override
-    protected double calcDoubleExpected(double operand_1, double operand_2) {
+    protected double calcExpected(double operand_1, double operand_2) {
         return operand_1 + operand_2;
     }
 }
--- src/test/java/com/company/calculator/NumberTest.java
+++ src/test/java/com/company/calculator/NumberTest.java
@@ -10,38 +10,24 @@ import static org.junit.Assert.assertEquals;
 public abstract class NumberTest {
     protected static NumberOperation numberOperation;
 
-    protected abstract int calcIntExpected(int operand_1, int  operand_2);
+    protected abstract double calcExpected(double operand_1, double  operand_2);
 
-    protected abstract double calcDoubleExpected(double operand_1, double  operand_2);
-
-
-    private void executeDoubleTest() throws Exception {
-        double operand_1 = Util.getRandomDouble();
-        double operand_2 = Util.getRandomDouble();
-
-        ArrayList<String> operands = new ArrayList<>();
-        operands.add(Double.toString(operand_1));
-        operands.add(Double.toString(operand_2));
-        numberOperation.setOperands(operands);
-
-        assertEquals(Double.toString(calcDoubleExpected(operand_1, operand_2)), numberOperation.execute());
+    private String calcExpected(String operand_1, String operand_2) {
+        return Numbers.numberConversation(Double.toString(
+                calcExpected(Double.parseDouble(operand_1), Double.parseDouble(operand_2))));
     }
 
-    private void executeIntegerTest() throws Exception {
-        int operand_1 = Util.getRandomInteger();
-        int operand_2 = Util.getRandomInteger();
-
-        ArrayList<String> operands = new ArrayList<>();
-        operands.add(Integer.toString(operand_1));
-        operands.add(Integer.toString(operand_2));
-        numberOperation.setOperands(operands);
-
-        assertEquals(Integer.toString(calcIntExpected(operand_1, operand_2)), numberOperation.execute());
-    }
 
     @org.junit.Test(timeout = 1000)
     public void executeTest() throws Exception {
-        executeIntegerTest();
-        executeDoubleTest();
+        String operand_1 = Util.generateOperand();
+        String operand_2 = Util.generateOperand();
+
+        ArrayList<String> operands = new ArrayList<>();
+        operands.add(operand_1);
+        operands.add(operand_2);
+        numberOperation.setOperands(operands);
+
+        assertEquals(calcExpected(operand_1, operand_2), numberOperation.execute());
     }
 }
