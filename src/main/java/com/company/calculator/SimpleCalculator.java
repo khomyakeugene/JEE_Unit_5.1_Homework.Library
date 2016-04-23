48aed414bd29348e03ec169ee46185ef359c371e
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 16:46:10 +0300

    JEE Unit 5.1 Homework: Library

--- src/main/java/com/company/calculator/SimpleCalculator.java
+++ src/main/java/com/company/calculator/SimpleCalculator.java
@@ -9,6 +9,10 @@ import java.util.Optional;
  * Created by Yevhen on 22.04.2016.
  */
 public class SimpleCalculator implements Calculator {
+    private static final String IMPOSSIBLE_TO_RECOGNIZE_OPERATION_SIGNATURE_PATTERN =
+            "Operation code has been detected as \"%s\", but it is impossible to recognize available "+
+                    "operation signature in the expression\n   \"%s\"";
+
     private static final String ADDITION_OPERATION_CODE = "+";
     private static final String SUBTRACT_OPERATION_CODE = "-";
 
@@ -28,12 +32,12 @@ public class SimpleCalculator implements Calculator {
 
     @Override
     public String execute(String inputExpression) {
-        String result = null;
+        String result;
 
         // Parse input expression: receive operation description in <parseResult> or IllegalArgumentException
         // if <inputExpression> is invalid
         ParseResult parseResult = parser.parse(operationMap.keySet(), inputExpression);
-        // Search suitable operation, always suppose that if such operation could be found more than one be more,
+        // Search suitable operation, always suppose that if such operation could be found more than one,
         // only the first one should be executed
         Optional<Operation> first = operationMap.get(parseResult.operationCode()).
                 stream().filter(o -> o.isThisOperation(inputExpression, parseResult)).findFirst();
@@ -43,6 +47,9 @@ public class SimpleCalculator implements Calculator {
             operation.setOperands(parseResult.operandList());
             // Execute operation
             result = operation.execute();
+        } else {
+            throw new IllegalArgumentException(String.format(IMPOSSIBLE_TO_RECOGNIZE_OPERATION_SIGNATURE_PATTERN,
+                    parseResult.operationCode(), inputExpression));
         }
 
         return result;
