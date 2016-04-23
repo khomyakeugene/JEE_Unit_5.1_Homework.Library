86aab24af50f3d005e1bf27aaa5bb51a32d0c7ef
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 13:13:06 +0300

    JEE Unit 5.1 Homework: Library

---
+++ src/test/java/com/company/calculator/ExpressionTest.java
@@ -0,0 +1,36 @@
+package com.company.calculator;
+
+import org.junit.Before;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by Yevhen on 23.04.2016.
+ */
+public class ExpressionTest {
+    protected static final String ADDITION_OPERATION_CODE = "+";
+    protected static final String SUBTRACT_OPERATION_CODE = "-";
+    protected static final String INVALID_OPERATION_CODE = "!!";
+    private static final String EXPRESSION_PATTERN = "%s %s %s";
+
+    protected String operand_1;
+    protected String operand_2;
+    protected String operationCode;
+    protected String validBinaryOperationExpression;
+    protected String invalidBinaryOperationExpression;
+
+    protected void generateTestData() {
+        operand_1 = Util.generateOperand();
+        operand_2 = Util.generateOperand();
+        operationCode = Util.getRandomBoolean() ? ADDITION_OPERATION_CODE : SUBTRACT_OPERATION_CODE;
+
+        validBinaryOperationExpression = String.format(EXPRESSION_PATTERN, operand_1, operationCode, operand_2);
+        invalidBinaryOperationExpression = String.format(EXPRESSION_PATTERN, operand_1, INVALID_OPERATION_CODE, operand_2);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        generateTestData();
+    }
+}
--- src/test/java/com/company/calculator/SimpleParserTest.java
+++ src/test/java/com/company/calculator/SimpleParserTest.java
@@ -1,6 +1,5 @@
 package com.company.calculator;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -13,39 +12,14 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by Yevhen on 23.04.2016.
  */
-public class SimpleParserTest {
-    private static final String ADDITION_OPERATION_CODE = "+";
-    private static final String SUBTRACT_OPERATION_CODE = "-";
-    private static final String INVALID_OPERATION_CODE = "!!";
-    private static final String EXPRESSION_PATTERN = "%s %s %s";
-
+public class SimpleParserTest extends ExpressionTest {
     private static SimpleParser simpleParser = new SimpleParser();
     private static Set<String> operationCodeSet = new TreeSet<>();
-
-    private String operand_1;
-    private String operand_2;
-    private String operationCode;
-    private String validBinaryOperationExpression;
-    private String invalidBinaryOperationExpression;
-
-    private void generateTestData() {
-        operand_1 = Util.generateOperand();
-        operand_2 = Util.generateOperand();
-        operationCode = Util.getRandomBoolean() ? ADDITION_OPERATION_CODE : SUBTRACT_OPERATION_CODE;
-
-        validBinaryOperationExpression = String.format(EXPRESSION_PATTERN, operand_1, operationCode, operand_2);
-        invalidBinaryOperationExpression = String.format(EXPRESSION_PATTERN, operand_1, INVALID_OPERATION_CODE, operand_2);
-    }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
         operationCodeSet.add(ADDITION_OPERATION_CODE);
         operationCodeSet.add(SUBTRACT_OPERATION_CODE);
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        generateTestData();
     }
 
     @Test(timeout = 1000)
