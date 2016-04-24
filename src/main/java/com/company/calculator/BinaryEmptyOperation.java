e15ef9961c2789e338938c6744ebac1156df6526
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-24 11:58:21 +0300

    JEE Unit 5.1 Homework: Library

---
+++ src/main/java/com/company/calculator/BinaryEmptyOperation.java
@@ -0,0 +1,12 @@
+package com.company.calculator;
+
+/**
+ * Created by Yevhen on 24.04.2016.
+ */
+public abstract class BinaryEmptyOperation extends EmptyOperation implements Operation {
+    @Override
+    public boolean isThisOperation(String s, ParseResult parseResult) {
+        // Check that there are only two operands
+        return parseResult.operandList().size() == 2;
+    }
+}
